6ae33072a564312380e5069bd5504050279772fc
arthur <arthur.cavalcante.puc@gmail.com>
2021-03-15 14:02:46 -0300

    modsecurity rules workinggit status :)

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/sirupsen/logrus v1.7.0
+	github.com/tsuru/nginx-operator v0.6.3
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/oauth2 v0.0.0-20210210192628-66670185b0cd // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
--- internal/handlers.go
+++ internal/handlers.go
@@ -7,13 +7,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/tsuru/nginx-operator/api/v1alpha1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-func (w *WafManager) deployNginx(ctx context.Context, opts DeployOpts, configMapName string) error {
+func (w *WafManager) deployNginx(ctx context.Context, opts DeployOpts, configMapName, wafConf string) error {
 	nginxGVR := schema.GroupVersionResource{Group: "nginx.tsuru.io", Version: "v1alpha1", Resource: "nginxes"}
 
 	nginx := &unstructured.Unstructured{
@@ -33,6 +34,15 @@ func (w *WafManager) deployNginx(ctx context.Context, opts DeployOpts, configMap
 				"service": map[string]interface{}{
 					"type": "NodePort",
 				},
+				// ExtraFiles references to additional files into a object in the cluster.
+				// These additional files will be mounted on `/etc/nginx/extra_files`.
+				"extraFiles": &v1alpha1.FilesRef{
+					wafConf,
+					map[string]string{
+						"rules.conf":       "rules.conf",
+						"modsecurity.conf": "modsecurity.conf",
+					},
+				},
 			},
 		},
 	}
@@ -45,8 +55,34 @@ func (w *WafManager) deployNginx(ctx context.Context, opts DeployOpts, configMap
 	return nil
 }
 
-func (w *WafManager) deployConf(ctx context.Context, opts DeployOpts, configMapName string) error {
+func (w *WafManager) deployConf(ctx context.Context, opts DeployOpts, configMapName, wafConfName string) error {
 	immutable := new(bool)
+	wafConf := &v1.ConfigMap{
+		metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		metav1.ObjectMeta{
+			Name:      wafConfName,
+			Namespace: opts.Namespace,
+		},
+
+		immutable,
+		map[string]string{
+			"modsecurity.conf": recomended,
+			"rules.conf": `
+# Include the recommended configuration
+Include /etc/nginx/extra_files/modsecurity.conf
+# A test rule
+SecRule ARGS:testparam "@contains test" "id:1234,deny,log,status:403"
+`,
+		},
+		nil,
+	}
+	_, err := w.defaultClient.CoreV1().ConfigMaps(opts.Namespace).Create(ctx, wafConf, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
 	configMap := &v1.ConfigMap{
 		metav1.TypeMeta{
 			Kind:       "ConfigMap",
@@ -60,11 +96,14 @@ func (w *WafManager) deployConf(ctx context.Context, opts DeployOpts, configMapN
 		immutable,
 		map[string]string{
 			"nginx.conf": `
+	load_module modules/ngx_http_modsecurity_module.so;
 	events {}
 
 	http {
 		server {
 		listen 8080;
+		modsecurity on;
+		modsecurity_rules_file /etc/nginx/extra_files/rules.conf;
 
 		location / {
 			proxy_pass http://go-hostname.backend.svc.cluster.local:80;
@@ -74,7 +113,7 @@ func (w *WafManager) deployConf(ctx context.Context, opts DeployOpts, configMapN
 		},
 		nil,
 	}
-	_, err := w.defaultClient.CoreV1().ConfigMaps(opts.Namespace).Create(ctx, configMap, metav1.CreateOptions{})
+	_, err = w.defaultClient.CoreV1().ConfigMaps(opts.Namespace).Create(ctx, configMap, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
@@ -91,10 +130,11 @@ func (w *WafManager) deploy(c echo.Context) error {
 	}
 
 	configMapName := fmt.Sprintf("%s-conf", opts.Name)
-	if err := w.deployConf(ctx, opts, configMapName); err != nil {
+	wafConf := fmt.Sprintf("%s-conf-extra", opts.Name)
+	if err := w.deployConf(ctx, opts, configMapName, wafConf); err != nil {
 		return err
 	}
-	if err := w.deployNginx(ctx, opts, configMapName); err != nil {
+	if err := w.deployNginx(ctx, opts, configMapName, wafConf); err != nil {
 		return err
 	}
 
