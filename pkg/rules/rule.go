b3cc0f875e0a979341d342b307d1791b6b8bdac1
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2021-04-09 14:43:12 -0300

    Feat dynamic rules (#1)
    * feat: rules are created dinamically in /etc/nginx/extrafiles/
    
    * refactor: rules filename, configmap names

---
+++ pkg/rules/rule.go
@@ -0,0 +1,40 @@
+package rules
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type WAFRule map[string]string
+
+func RenderRules() (WAFRule, error) {
+	var rules WAFRule = make(map[string]string)
+	root, err := os.Getwd()
+	if err != nil {
+		return WAFRule{}, err
+	}
+	rootPath := fmt.Sprintf("%s/rules", root)
+	err = filepath.Walk(rootPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			pathName := fmt.Sprintf("%s/%s", rootPath, info.Name())
+			content, err := os.ReadFile(pathName)
+			if err != nil {
+				return err
+			}
+
+			rules[info.Name()] = string(content)
+			return nil
+		})
+	if err != nil {
+		return WAFRule{}, err
+	}
+
+	return rules, nil
+}
